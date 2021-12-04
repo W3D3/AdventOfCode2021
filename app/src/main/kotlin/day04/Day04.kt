e5ca196c9a4938244ff975acaa9b169f9a14c7f6
Christoph Wedenig <christoph@wedenig.org>
2021-12-04 12:26:17 +0100

    WIP

--- app/src/main/kotlin/day04/Day04.kt
+++ app/src/main/kotlin/day04/Day04.kt
@@ -11,9 +11,30 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay04Part1, ::solveDay04Part2)
 }
 
+data class BingoTile(val value: Int, val marked: Boolean) {
+    override fun toString(): String {
+        return value.toString()
+    }
+}
 
 fun solveDay04Part1(input: List<String>): Int {
-    TODO()
+    var drawn = input.first().split(",").map(String::toInt)
+
+    var bingoBoards = input.drop(1).map { s ->
+        if (s != "") {
+            s
+        } else {
+            null
+        }
+    }.filterNotNull()
+        .map { s -> s.split("\\s+".toRegex()).map { numStr -> BingoTile(numStr.toInt(), false) } }
+        .chunked(5);
+
+    println(bingoBoards)
+
+
+
+    return 0
 }
 
 fun solveDay04Part2(input: List<String>): Int {
