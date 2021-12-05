ac725f0c05c42828ba826cb1fc6eca7467598943
Christoph Wedenig <christoph@wedenig.org>
2021-12-05 13:44:31 +0100

    Day 5 done, refactor later

--- app/src/main/kotlin/day05/Day05.kt
+++ app/src/main/kotlin/day05/Day05.kt
@@ -3,6 +3,7 @@ package day05
 import common.InputRepo
 import common.readSessionCookie
 import common.solve
+import kotlin.math.abs
 
 fun main(args: Array<String>) {
     val day = 5
@@ -11,11 +12,60 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay05Part1, ::solveDay05Part2)
 }
 
+data class Line(val beginning: Coord, val end: Coord) {
+    fun isNonDiagonal(): Boolean {
+        return (beginning.x == end.x) or (beginning.y == end.y)
+    }
+
+    fun getAffectedCoords(): Collection<Coord> {
+        val affectedCoords = ArrayList<Coord>()
+
+        val step_x = if (beginning.x < end.x) 1 else if (beginning.x == end.x) 0 else -1
+        val step_y = if (beginning.y < end.y) 1 else if (beginning.y == end.y) 0 else -1
+
+        val x_diff = beginning.x - end.x
+        val y_diff = beginning.y - end.y
+        // lmao 45 degree hax below
+        val distance: Int = (abs(x_diff) + abs(y_diff)) / if (isNonDiagonal()) 1 else 2
+        for (i in 0..distance) {
+            affectedCoords.add(Coord(beginning.x + step_x * i, beginning.y + step_y * i))
+        }
+
+        return affectedCoords
+    }
+}
+
+data class Coord(val x: Int, val y: Int)
+
+fun parseLine(line: String): Line {
+    val regex = """(\d+),(\d+) -> (\d+),(\d+)""".toRegex()
+    val matchResult = regex.find(line)
+    val (x1, y1, x2, y2) = matchResult!!.destructured
+
+    return Line(Coord(x1.toInt(), y1.toInt()), Coord(x2.toInt(), y2.toInt()))
+}
 
 fun solveDay05Part1(input: List<String>): Int {
-    TODO()
+    val coverMap = HashMap<Coord, Int>()
+
+    input.map { line -> parseLine(line) }
+            .filter(Line::isNonDiagonal)
+            .forEach { line ->
+                line.getAffectedCoords()
+                        .forEach { coord -> coverMap.compute(coord) { _, v -> if (v == null) 1 else v + 1 } }
+            }
+
+    return coverMap.count { entry -> entry.value >= 2 }
 }
 
 fun solveDay05Part2(input: List<String>): Int {
-    TODO()
+    val coverMap = HashMap<Coord, Int>()
+
+    input.map { line -> parseLine(line) }
+            .forEach { line ->
+                line.getAffectedCoords()
+                        .forEach { coord -> coverMap.compute(coord) { _, v -> if (v == null) 1 else v + 1 } }
+            }
+
+    return coverMap.count { entry -> entry.value >= 2 }
 }
--- app/src/test/kotlin/day05/Day05Test.kt
+++ app/src/test/kotlin/day05/Day05Test.kt
@@ -8,11 +8,21 @@ import io.kotest.matchers.shouldBe
 class Day05Test : FreeSpec({
 
     val sampleInput: List<String> = listOf(
+            "0,9 -> 5,9",
+            "8,0 -> 0,8",
+            "9,4 -> 3,4",
+            "2,2 -> 2,1",
+            "7,0 -> 7,4",
+            "6,4 -> 2,0",
+            "0,9 -> 2,9",
+            "3,4 -> 1,4",
+            "0,0 -> 8,8",
+            "5,5 -> 8,2",
     )
 
-    val sampleSolutionPart1: Int = -1
+    val sampleSolutionPart1 = 5
 
-    val sampleSolutionPart2: Int = -1
+    val sampleSolutionPart2 = 12
 
     "Solving day 5" - {
         "part 1 for the sample input should return the correct output" {
