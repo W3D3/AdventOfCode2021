507d2b727884b22e80adb4f7984a4da3be3e2e1f
Christoph Wedenig <christoph.wedenig@dynatrace.com>
2021-12-07 20:39:28 +0100

    Solved Day 7

--- app/src/main/kotlin/day07/Day07.kt
+++ app/src/main/kotlin/day07/Day07.kt
@@ -3,6 +3,9 @@ package day07
 import common.InputRepo
 import common.readSessionCookie
 import common.solve
+import java.util.Collections.max
+import java.util.Collections.min
+import kotlin.math.abs
 
 fun main(args: Array<String>) {
     val day = 7
@@ -13,9 +16,62 @@ fun main(args: Array<String>) {
 
 
 fun solveDay07Part1(input: List<String>): Int {
-    TODO()
+    return median(input)
+//    return bruteforce(input)
+}
+
+private fun bruteforce(input: List<String>): Int {
+    val positions = parsePositions(input)
+
+    var min: Int = Int.MAX_VALUE
+
+    for (position in positions) {
+        val sumOf = positions.sumOf { abs(it - position) }
+        if (sumOf < min) {
+            min = sumOf;
+        }
+    }
+
+    return min
+}
+
+fun median(input: List<String>): Int {
+    val positions = parsePositions(input)
+
+    var median = positions.sorted()[positions.size / 2]
+
+    return positions.sumOf { abs(it - median) }
 }
 
 fun solveDay07Part2(input: List<String>): Int {
-    TODO()
+    return bruteforce2(input);
 }
+
+private fun bruteforce2(input: List<String>): Int {
+    val positions = parsePositions(input).sorted()
+
+    var min: Int = Int.MAX_VALUE
+
+    print(positions)
+    for (goal in min(positions)..max(positions)) {
+        val sumOf = positions.sumOf { pos ->
+            val sum = generateSequence(0) { if (it < abs(pos - goal)) it + 1 else null }.sum()
+            println("$sum fuel for goal $goal from $pos")
+            sum
+        }
+        println("===============================")
+        println("Total: $sumOf")
+        if (sumOf < min) {
+            min = sumOf;
+            println("NEW MIN: $sumOf")
+        } else {
+            // no new min in the sorted list means, we're done
+            break
+        }
+    }
+
+    return min
+}
+
+private fun parsePositions(input: List<String>) =
+    input.map { line -> line.split(",").map(String::toInt) }.flatten()
--- app/src/test/kotlin/day07/Day07Test.kt
+++ app/src/test/kotlin/day07/Day07Test.kt
@@ -4,15 +4,15 @@ import io.kotest.core.annotation.Ignored
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
-@Ignored
 class Day07Test : FreeSpec({
 
     val sampleInput: List<String> = listOf(
+        "16,1,2,0,4,2,7,1,2,14"
     )
 
-    val sampleSolutionPart1: Int = -1
+    val sampleSolutionPart1: Int = 37
 
-    val sampleSolutionPart2: Int = -1
+    val sampleSolutionPart2: Int = 168
 
     "Solving day 7" - {
         "part 1 for the sample input should return the correct output" {
