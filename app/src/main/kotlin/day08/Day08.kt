e527efcc8b81ea44945c1ce7025c816c00de759a
Christoph Wedenig <christoph@wedenig.org>
2021-12-08 20:50:56 +0100

    Andi style comments

--- app/src/main/kotlin/day08/Day08.kt
+++ app/src/main/kotlin/day08/Day08.kt
@@ -49,20 +49,16 @@ private fun generateNumToPattern(sizeToPattern: Map<Int, List<Set<Char>>>): Hash
     numToPattern[4] = sizeToPattern[4]!!.first()
     numToPattern[7] = sizeToPattern[3]!!.first()
     numToPattern[8] = sizeToPattern[7]!!.first()
-    // Alias 1 to rightLine, to make things more readable
-    val rightLine = numToPattern[1]!!
-    // 3 is the only 5 line digit with the right line turned on
-    numToPattern[3] = sizeToPattern[5]!!.first { chars -> chars.containsAll(rightLine) }
-    // 5 is the only 5 line digit with a top L shape
+
+    val rightLine = numToPattern[1]!!   // Alias 1 to rightLine, to make things more readable
+
+    numToPattern[3] = sizeToPattern[5]!!.first { chars -> chars.containsAll(rightLine) }    // 3 is the only 5 line digit with the right line turned on
+
     val topL = numToPattern[4]!! subtract rightLine
-    numToPattern[5] = sizeToPattern[5]!!.first { chars -> chars.containsAll(topL) }
-    // 2 is the only 5 line digit left
-    numToPattern[2] = (sizeToPattern[5]!! subtract setOf(numToPattern[5]!!, numToPattern[3]!!)).first()
-    // 9 is the only 6 line digit with a 4 shape in it
-    numToPattern[9] = sizeToPattern[6]!!.first { chars -> chars.containsAll(numToPattern[4]!!) }
-    // 0 is the only other 6 line digit with a right line (that is not 9)
-    numToPattern[0] = sizeToPattern[6]!!.first { chars -> (chars != numToPattern[9]) and chars.containsAll(rightLine) }
-    // 6 is the only 6 line digit left
-    numToPattern[6] = sizeToPattern[6]!!.subtract(setOf(numToPattern[9]!!, numToPattern[0]!!)).first()
+    numToPattern[5] = sizeToPattern[5]!!.first { chars -> chars.containsAll(topL) }                                         // 5 is the only 5 line digit with a top L shape
+    numToPattern[2] = (sizeToPattern[5]!! subtract setOf(numToPattern[5]!!, numToPattern[3]!!)).first()                     // 2 is the only 5 line digit left
+    numToPattern[9] = sizeToPattern[6]!!.first { chars -> chars.containsAll(numToPattern[4]!!) }                            // 9 is the only 6 line digit with a 4 shape in it
+    numToPattern[0] = sizeToPattern[6]!!.first { chars -> (chars != numToPattern[9]) and chars.containsAll(rightLine) }     // 0 is the only other 6 line digit with a right line (that is not 9)
+    numToPattern[6] = sizeToPattern[6]!!.subtract(setOf(numToPattern[9]!!, numToPattern[0]!!)).first()                      // 6 is the only 6 line digit left
     return numToPattern
 }
