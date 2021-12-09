4ff089180cdf81951cbbc46cd5e285b94b1dda3d
Christoph Wedenig <christoph.wedenig@dynatrace.com>
2021-12-09 17:52:13 +0100

    Solved Day 9

--- app/src/main/kotlin/day09/Day09.kt
+++ app/src/main/kotlin/day09/Day09.kt
@@ -1,6 +1,7 @@
 package day09
 
 import common.InputRepo
+import common.colored
 import common.readSessionCookie
 import common.solve
 
@@ -11,11 +12,79 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay09Part1, ::solveDay09Part2)
 }
 
+private fun parseInput(input: List<String>): List<List<Int>> {
+    return input.map { line -> line.map { c -> c.digitToInt() } }
+}
+
+data class Point(val x: Int, val y: Int, val height: Int)
+
+private fun getLowPoints(map: List<List<Int>>, print: Boolean = false): ArrayList<Point> {
+    val lowPoints = ArrayList<Point>()
+    for (i in map.indices) {
+        for (j in map[i].indices) {
+            val currentPoint = map[i][j]
+            val neighbors = setOf(
+                map.getOrNull(i + 1)?.getOrNull(j),
+                map.getOrNull(i)?.getOrNull(j + 1),
+                map.getOrNull(i - 1)?.getOrNull(j),
+                map.getOrNull(i)?.getOrNull(j - 1),
+            ).filterNotNull()
+
+            if (neighbors.all { neighbor -> neighbor > currentPoint }) {
+                lowPoints.add(Point(i, j, currentPoint))
+                if (print) colored { print(currentPoint.toString().red) }
+            } else {
+                if (print) print(currentPoint)
+            }
+        }
+        if (print) println()
+    }
+    return lowPoints
+}
+
 
 fun solveDay09Part1(input: List<String>): Int {
-    TODO()
+    val heightMap = parseInput(input)
+
+    return getLowPoints(heightMap).sumOf { p -> p.height + 1 }
+}
+
+private fun findBasin(map: List<List<Int>>, point: Point, visited: Set<Point>): Set<Point> {
+    val i = point.x
+    val j = point.y
+
+    var newVisited: Set<Point>
+    val unvisitedNeighbors = setOf(
+        pointAt(map, i + 1, j),
+        pointAt(map, i, j + 1),
+        pointAt(map, i - 1, j),
+        pointAt(map, i, j - 1),
+    )
+        .filterNotNull()
+        .filter { value -> value.height < 9 }
+        .filter { value -> !visited.contains(value) }
+
+    if (unvisitedNeighbors.isEmpty())
+        return emptySet()
+
+    newVisited = visited union unvisitedNeighbors union setOf(point)
+    for (neighbor in unvisitedNeighbors) {
+        newVisited = newVisited.plus(findBasin(map, neighbor, newVisited))
+    }
+    return newVisited
+}
+
+private fun pointAt(map: List<List<Int>>, x: Int, y: Int): Point? {
+    val value = map.getOrNull(x)?.getOrNull(y) ?: return null
+    return Point(x, y, value)
 }
 
 fun solveDay09Part2(input: List<String>): Int {
-    TODO()
+    val heightMap = parseInput(input)
+
+    val lowPoints = getLowPoints(heightMap)
+
+    return lowPoints.map { p ->
+        findBasin(heightMap, p, mutableSetOf(p)).size
+    }.sortedDescending().take(3).reduce { acc, i -> acc * i }
 }
