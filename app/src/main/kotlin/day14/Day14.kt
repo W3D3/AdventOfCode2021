df4f21cd99838150e703bcd0ffa4f8b5532c7b8c
Christoph Wedenig <christoph@wedenig.org>
2021-12-15 22:19:37 +0100

    WIP day 14

--- app/src/main/kotlin/day14/Day14.kt
+++ app/src/main/kotlin/day14/Day14.kt
@@ -4,6 +4,22 @@ import common.InputRepo
 import common.readSessionCookie
 import common.solve
 
+
+data class Input(val template: String, val rules: Map<String, String>)
+
+private fun parseInput(input: List<String>): Input {
+    val rulesRegex = """(\w+) -> (\w+)""".toRegex()
+
+    val (templateRaw, foldsRaw) = input.partition { (it != "") and !it.contains("->") }
+    val template = templateRaw.first()
+    val rules = foldsRaw.associate {
+        println(it)
+        val (from, to) = rulesRegex.find(it)!!.destructured
+        from to to
+    }
+    return Input(template, rules)
+}
+
 fun main(args: Array<String>) {
     val day = 14
     val input = InputRepo(args.readSessionCookie()).get(day = day)
@@ -12,8 +28,13 @@ fun main(args: Array<String>) {
 }
 
 
-fun solveDay14Part1(input: List<String>): Int {
-    TODO()
+fun solveDay14Part1(input: List<String>): Long {
+    val (template, rules) = parseInput(input)
+
+    var countPairs: HashMap<String, Long>
+    repeat(10) {
+
+    }
 }
 
 fun solveDay14Part2(input: List<String>): Int {
